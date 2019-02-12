59ae734a0f33ae37343a075c203eb92b5d4341de
kamrul <kamrul_sscis@hotmail.com>
2019-02-12 13:15:10 +0000

    Updating overview component in group.

--- app/js/common/containers/group/edit/Overview.js
+++ app/js/common/containers/group/edit/Overview.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router';
-import { Slider } from '@xanda/react-components';
+import {Link} from 'react-router';
+import {Slider} from '@xanda/react-components';
 
 export default class Overview extends React.PureComponent {
 
-	render() {
-    const { positionX, positionY } = this.props
+    render() {
+        const {positionX, positionY, icon, title, icon_path, icon_size} = this.props
+        console.log('All props', this.props);
 
-		return (
-                <div class="success-add-group">
-                        <div class="success-message">
-                            <p>Great, you're ready to add group!</p>
-                        </div>  
-                        
-                        <div class="royalty-wrapper">
-                            <p>Royalty</p>
-                            <span>{positionY}</span>
-                        </div>
-                        
-                        <div class="loyalty-wrapper">
-                            <p>Loyalty</p>
-                            <span>{positionX}</span>
-                        </div>                               
-                        
+        return (
+            <div class="success-add-group">
+                <div className="success-message">
+                    <p>Great, you're ready to add group!</p>
                 </div>
-		);
-	}
+
+                <div className="ov-title">
+                    {title}
+                </div>
+
+                <div className="ov-img-content">
+                    <div className="ov-icon-path"><img src={icon ? icon.preview : icon_path}/></div>
+                    <div className="ov-icon-size">{icon_size}</div>
+                </div>
+
+                <div className="royalty-wrapper">
+                    <p>Royalty</p>
+                    <span>{positionY}</span>
+                </div>
+
+                <div className="loyalty-wrapper">
+                    <p>Loyalty</p>
+                    <span>{positionX}</span>
+                </div>
+
+            </div>
+        );
+    }
 }
