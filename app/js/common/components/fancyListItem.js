7ccd8f6254f324b5e5100bd5a61f941617541403
kamrul <kamrul_sscis@hotmail.com>
2019-02-12 13:24:20 +0000

    Adding fancy listitem icon path & icon

--- app/js/common/components/fancyListItem.js
+++ app/js/common/components/fancyListItem.js
@@ -2,30 +2,22 @@ import React from 'react';
 
 export default class FancyListItem extends React.PureComponent {
 
-	render() {
-    const { children, key, className, actions, icon } = this.props
+    render() {
+        const {children, key, className, actions, icon, iconPath} = this.props
 
-		return (
-      <li
-        className={`fancylist-item ${className}`}
-        key={key}
-      >
-        {icon &&
-          <span className="fancylist-item-icon">
-            {icon}
-          </span>
-        }
-        <span className="fancylist-item-title">
-          {children}
-        </span>
-        {actions &&
-          <span className="fancylist-item-actions">
-            {actions &&
-              actions
-            }
-          </span>
-        }
-      </li>
-		);
-	}
+        return (
+            <li
+                className={`fancylist-item ${className}`}
+                key={key}
+            >
+                {icon && <span className="fancylist-item-icon-size">{icon}</span>}
+
+                {iconPath && <span className="fancylist-item-icon-path"><img src={iconPath}/></span>}
+
+                <span className="fancylist-item-title">{children}</span>
+
+                {actions && <span className="fancylist-item-actions">{actions && actions}</span>}
+            </li>
+        );
+    }
 }
--- app/js/common/containers/group/List.js
+++ app/js/common/containers/group/List.js
@@ -1,113 +1,115 @@
 import React from 'react';
-import { Link } from 'react-router';
-import { fetchData } from 'app/actions';
-import { connect } from 'react-redux';
-import { Form, Checkbox, ContentLoader } from '@xanda/react-components';
-import { Popup, FancyList, FancyListItem } from 'app/components';
-import { api } from 'app/utils';
-import { url } from 'app/constants';
-import { makeGetProject, makeGetProjects } from 'app/containers/project/selector';
+import {Link} from 'react-router';
+import {fetchData} from 'app/actions';
+import {connect} from 'react-redux';
+import {Form, Checkbox, ContentLoader} from '@xanda/react-components';
+import {Popup, FancyList, FancyListItem} from 'app/components';
+import {api} from 'app/utils';
+import {url} from 'app/constants';
+import {makeGetProject, makeGetProjects} from 'app/containers/project/selector';
 
 @connect((state, ownProps) => {
-	const getProjects = makeGetProjects();
-	const getProject = makeGetProject();
-	return {
-		projects: getProjects(state),
-		project: getProject(state, ownProps.params.id),
-	};
+    const getProjects = makeGetProjects();
+    const getProject = makeGetProject();
+    return {
+        projects: getProjects(state),
+        project: getProject(state, ownProps.params.id),
+    };
 })
 export default class List extends React.PureComponent {
-	fetchData = () => {
-		this.props.dispatch(fetchData({
-			type: 'PROJECT',
-			url: `/projects/${this.props.params.id}`,
-		}));
-	}
-
-  handleStatusChange = async (group, newStatus) => {
-    // check status has changed
-    const status = newStatus === "1" ? 1 : 0
-    if(group.status !== status){
-      const response = await api.put(`groups/${group.id}`, { status:status })
-      if(!api.error(response)){
-        this.fetchData()
-      }
-    }
-  }
-
-  handleDelete = async (groupId) => {
-    if (window.confirm("Are you sure you want to delete this group?")) {
-      const response = await api.delete(`groups/${groupId}`)
-      if(!api.error(response)){
-        this.props.dispatch({type: 'GROUP_DELETED', payload: response.data})
-      }
-    }
-  }
-
-  renderItem = (group) => {
-    if(!group){
-      return
+    fetchData = () => {
+        this.props.dispatch(fetchData({
+            type: 'PROJECT',
+            url: `/projects/${this.props.params.id}`,
+        }));
     }
 
-    return (
-      <FancyListItem
-        key={group.id}
-        className={(group.status === 1) ? `is-active` : `is-inactive`}
-        //icon={"X"}
-        actions={
-          <React.Fragment>
-            <Checkbox
-              name="status"
-              value={group.status}
-              options={[
-                {
-                  id: 1,
-                  title: 'Visible'
-                }
-              ]}
-              onChange={(name, value) => this.handleStatusChange(group, value)}
-              styled
-              className="switch"
-            />
-            <Link to={`/${url.projects}/${this.props.params.id}/${url.groups}/${group.id}/edit`} className="icon-edit" />
-            <span onClick={() => this.handleDelete(group.id)} className="icon-bin" />
-          </React.Fragment>
+    handleStatusChange = async (group, newStatus) => {
+        // check status has changed
+        const status = newStatus === "1" ? 1 : 0
+        if (group.status !== status) {
+            const response = await api.put(`groups/${group.id}`, {status: status})
+            if (!api.error(response)) {
+                this.fetchData()
+            }
         }
-      >
-        {group.title}
-      </FancyListItem>
+    }
 
-    )
-  }
+    handleDelete = async (groupId) => {
+        if (window.confirm("Are you sure you want to delete this group?")) {
+            const response = await api.delete(`groups/${groupId}`)
+            if (!api.error(response)) {
+                this.props.dispatch({type: 'GROUP_DELETED', payload: response.data})
+            }
+        }
+    }
 
-	render() {
+    renderItem = (group) => {
+        if (!group) {
+            return
+        }
 
+        return (
+            <FancyListItem
+                key={group.id}
+                className={(group.status === 1) ? `is-active` : `is-inactive`}
+                icon={group.icon_size}
+                iconPath={group.icon_path}
+                actions={
+                    <React.Fragment>
+                        <Checkbox
+                            name="status"
+                            value={group.status}
+                            options={[
+                                {
+                                    id: 1,
+                                    title: 'Visible'
+                                }
+                            ]}
+                            onChange={(name, value) => this.handleStatusChange(group, value)}
+                            styled
+                            className="switch"
+                        />
+                        <Link to={`/${url.projects}/${this.props.params.id}/${url.groups}/${group.id}/edit`}
+                              className="icon-edit"/>
+                        <span onClick={() => this.handleDelete(group.id)} className="icon-bin"/>
+                    </React.Fragment>
+                }
+            >
+                {group.title}
+            </FancyListItem>
 
-		const { projects, project } = this.props
+        )
+    }
 
-		return (
-			<Popup
-        title="Groups"
-				closePath={`/${url.projects}/${this.props.params.id}`}
-        buttons={[
-          <Link className="button" to={`/${url.projects}/${this.props.params.id}/${url.groups}/add`}>Add group</Link>
-        ]}
-			>
-        <ContentLoader
-          data={projects.collection}
-          isLoading={projects.isLoading}
-        >
-  				{_.isEmpty(project.groups) ? (
-  					<Link to={`/${url.projects}/${this.props.params.id}/${url.groups}/add`}>Add your first group <span dangerouslySetInnerHTML={{__html: `&plus;`}} /></Link>
-  				) : (
-  					<React.Fragment>
-  						<FancyList>
-  							{_.map(project.groups, (group) => this.renderItem(group))}
-  						</FancyList>
-  					</React.Fragment>
-  				)}
-        </ContentLoader>
-			</Popup>
-		);
-	}
+    render() {
+        const {projects, project} = this.props
+
+        return (
+            <Popup
+                title="Groups"
+                closePath={`/${url.projects}/${this.props.params.id}`}
+                buttons={[
+                    <Link className="button" to={`/${url.projects}/${this.props.params.id}/${url.groups}/add`}>Add
+                        group</Link>
+                ]}
+            >
+                <ContentLoader
+                    data={projects.collection}
+                    isLoading={projects.isLoading}
+                >
+                    {_.isEmpty(project.groups) ? (
+                        <Link to={`/${url.projects}/${this.props.params.id}/${url.groups}/add`}>Add your first
+                            group <span dangerouslySetInnerHTML={{__html: `&plus;`}}/></Link>
+                    ) : (
+                        <React.Fragment>
+                            <FancyList>
+                                {_.map(project.groups, (group) => this.renderItem(group))}
+                            </FancyList>
+                        </React.Fragment>
+                    )}
+                </ContentLoader>
+            </Popup>
+        );
+    }
 }
