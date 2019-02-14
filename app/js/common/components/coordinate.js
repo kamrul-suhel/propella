d8df2e6389d56faa8aad618226479a9de9bf4df9
Will Samuel <Will Samuel@xandahq.local>
2019-02-14 12:40:13 +0000

    worked on progress grid

--- app/js/common/components/coordinate.js
+++ app/js/common/components/coordinate.js
@@ -23,7 +23,7 @@ export default class Coordinate extends React.PureComponent {
                     angleRadians = Math.floor(angleRadians);
 
                     // angle in degrees
-                    angleDeg = Math.atan2(nextCoordinate.positionY - coordinate.positionY, nextCoordinate.positionX - coordinate.positionX) * 180 / Math.PI;
+                    angleDeg = Math.atan2(coordinate.positionY - nextCoordinate.positionY, coordinate.positionX - nextCoordinate.positionX) * 180 / Math.PI;
                     angleDeg = Math.floor(angleDeg);
 
                     // Distance between 2 points
@@ -32,7 +32,7 @@ export default class Coordinate extends React.PureComponent {
 
                     c = Math.sqrt( a*a + b*b );
 
-                    console.log('angle in degrees : ', angleDeg);
+                    console.log('angle in degrees : ', angleRadians);
                     console.log('distance between : ', c);
                 }
 
@@ -40,7 +40,7 @@ export default class Coordinate extends React.PureComponent {
                     <div key={coordinate.id}
                          className="selected-group-wrapper progress-button"
                          style={{top: `${coordinate.positionX}%`, left: `${coordinate.positionY}%`}}>
-                        <div class="connector" style={{transform: `rotate(${angleDeg ? angleDeg : 0}deg)`, height: `${c ? c : 0}VH`}}></div>
+                        <div className="connector" style={{transform: `rotate(${angleDeg ? angleDeg : 0}deg)`, height: `${c ? c : 0}VH`}}></div>
                         <p><img src={coordinate.icon_path}/></p>
                     </div>
                 )
