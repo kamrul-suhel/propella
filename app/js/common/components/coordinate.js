94b30c9f20c51db060e2f5d4ab9890273dd827eb
kamrul <kamrul_sscis@hotmail.com>
2019-02-15 09:58:07 +0000

    Fixing coordinate connector WIP

--- app/js/common/components/coordinate.js
+++ app/js/common/components/coordinate.js
@@ -5,29 +5,35 @@ export default class Coordinate extends React.PureComponent {
     state = {}
 
     getDegreeRotate(currentCoordinate, nextCoordinate) {
-        let angleDeg = 0;
-        let c = 0;
-        console.log("current coordinate : ", currentCoordinate);
-        console.log("next coordinate : ", nextCoordinate);
+        let angleDeg = 0
+        let c = 0
+        const container = this.getContainer()
+        console.log('CUrrent coordinate: ', currentCoordinate)
+        console.log('Next coordinate : ', nextCoordinate)
 
         if (nextCoordinate) {
+            const cPositionX = container.width / 100 * currentCoordinate.positionX
+            const cPositionY = container.height / 100 * currentCoordinate.positionY
+            const nPositionX = container.width / 100 * nextCoordinate.positionX
+            const nPositionY = container.height / 100 * nextCoordinate.positionY
+
             // angle in degrees
-            angleDeg = Math.atan2(currentCoordinate.positionY - nextCoordinate.positionY, currentCoordinate.positionX - nextCoordinate.positionX) * 180 / Math.PI;
+            angleDeg = Math.atan2(cPositionY - nPositionY, cPositionX - nPositionX) * 180 / Math.PI
 
             // Change Positive Number to Negative Number
-            // if (Math.sign(angleDeg) === 1) {
-            //     angleDeg = Math.abs(angleDeg) * -1;
-            // } else {
-            //     angleDeg = Math.abs(angleDeg);
-            // }
+            if (Math.sign(angleDeg) === 1) {
+                angleDeg = Math.abs(angleDeg) * -1
+            } else {
+                angleDeg = Math.abs(angleDeg)
+            }
 
-            console.log('Angle degree : ', angleDeg);
+            console.log('Angle degree : ', angleDeg)
 
             // Distance between 2 points
-            let a = currentCoordinate.positionX - nextCoordinate.positionX;
-            let b = currentCoordinate.positionY - nextCoordinate.positionY;
+            let a = cPositionX - nPositionX
+            let b = cPositionY - nPositionY
 
-            c = Math.sqrt(a * a + b * b);
+            c = Math.sqrt(a * a + b * b)
         }
 
         return {
@@ -36,14 +42,22 @@ export default class Coordinate extends React.PureComponent {
         }
     }
 
-    renderRootCoordinate(group, position) {
+    getContainer = () => {
         const container = document.getElementById('gridwrapper-inner')
         const containerHeight = (container || {}).offsetHeight || 0
         const containerWidth = (container || {}).offsetWidth || 0
+        return {
+            height: containerHeight,
+            width: containerWidth
+        }
+    }
+
+    renderRootCoordinate(group, position) {
+        const container = this.getContainer()
 
         return (
             <div className="selected-group-wrapper first-coordinate progress-button"
-                 style={{transform: `translate(${containerWidth / 100 * group.positionX}px, ${containerHeight / 100 * group.positionY}px)`}}>
+                 style={{transform: `translate(${container.width / 100 * group.positionX}px, ${container.height / 100 * group.positionY}px)`}}>
                 <div className="connector" style={{
                     transform: `rotate(${position.degree ? 360 - position.degree : 0}deg)`,
                     width: `${position.height ? position.height : 0}VH`
@@ -59,8 +73,8 @@ export default class Coordinate extends React.PureComponent {
         return (
             group.coordinates && _.map(group.coordinates, (coordinate, index) => {
                 // Next coordinate
-                const nextCoordinate = group.coordinates[index + 1] && group.coordinates[index + 1];
-                const position = this.getDegreeRotate(coordinate, nextCoordinate);
+                const nextCoordinate = group.coordinates[index + 1] && group.coordinates[index + 1]
+                const position = this.getDegreeRotate(coordinate, nextCoordinate)
                 return (
                     <div key={coordinate.id}
                          className="selected-group-wrapper progress-button"
@@ -77,10 +91,10 @@ export default class Coordinate extends React.PureComponent {
     }
 
     render() {
-        const {group} = this.props;
+        const {group} = this.props
 
-        const firstCoordinate = group.coordinates[0] && group.coordinates[0];
-        const rootConnector = this.getDegreeRotate(group, firstCoordinate);
+        const firstCoordinate = group.coordinates[0] && group.coordinates[0]
+        const rootConnector = this.getDegreeRotate(group, firstCoordinate)
 
         return (
             <React.Fragment>
