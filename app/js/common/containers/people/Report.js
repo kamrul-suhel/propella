8e5022e7b3dc3273bd9c55542371398d101e9536
Jason Green <jason@xanda.netl>
2019-02-28 16:09:09 +0000

    Fixed compeititors loading and report backlinks

--- app/js/common/containers/people/Report.js
+++ app/js/common/containers/people/Report.js
@@ -1,52 +1,53 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { ReportList } from 'app/components';
-import { fetchData } from 'app/actions';
-import { makeGetGroup, makeGetGroups } from 'app/containers/group/selector';
-import { Nav } from 'app/components';
+import React from "react";
+import { connect } from "react-redux";
+import { ReportList } from "app/components";
+import { fetchData } from "app/actions";
+import { makeGetGroup, makeGetGroups } from "app/containers/group/selector";
+import { Nav } from "app/components";
 
 @connect((state, ownProps) => {
-    const getGroups = makeGetGroups();
-    const getGroup = makeGetGroup();
-    return {
-        groups: getGroups(state),
-        group: getGroup(state, ownProps.params.groupId),
-    };
+  const getGroups = makeGetGroups();
+  const getGroup = makeGetGroup();
+  return {
+    groups: getGroups(state),
+    group: getGroup(state, ownProps.params.groupId)
+  };
 })
 export default class Report extends React.PureComponent {
   componentDidMount() {
-      this.fetchData();
+    this.fetchData();
   }
 
   fetchData = () => {
-      this.props.dispatch(fetchData({
-          type: 'GROUP',
-          url: `/groups/${this.props.params.groupId}`,
-      }));
+    this.props.dispatch(
+      fetchData({
+        type: "GROUP",
+        url: `/groups/${this.props.params.groupId}`
+      })
+    );
+  };
+
+  handleReportPrint = () => {
+    window.print();
+  };
+
+  render() {
+    const { group, params } = this.props;
+
+    const downloadUrl = `groups/${this.props.params.groupId}/people?format_type=csv`;
+
+    return _.isEmpty(group.organisations) ? (
+      <h2>No people found</h2>
+    ) : (
+      <React.Fragment>
+        <Nav {...this.props} />
+        <ReportList
+          title="Data visual: People"
+          reportType="people"
+          data={group}
+          params={params}
+        />
+      </React.Fragment>
+    );
   }
-
-    handleReportPrint = () => {
-        window.print();
-    }
-
-    render() {
-        const {group, params} = this.props
-
-        const downloadUrl = `groups/${this.props.params.groupId}/people?format_type=csv`;
-
-        return (
-            (_.isEmpty(group.organisations) ? (
-                <h2>No people found</h2>
-            ) : (
-            <React.Fragment>
-                <Nav {...this.props} />
-                <ReportList
-                    reportType="people"
-                    data={group}
-                    params={params}/>
-            </React.Fragment>
-            ))
-            
-        );
-    }
 }
--- app/js/common/reducers/index.js
+++ app/js/common/reducers/index.js
@@ -1,41 +1,46 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import { combineReducers } from "redux";
+import { routerReducer } from "react-router-redux";
 
 // reducers
-import alert from './alert'
-import me from './me'
-import popup from './popup'
-import { project, projectUser } from 'app/containers/project/reducer'
-import group from 'app/containers/group/reducer'
-import { organisation, organisationType } from 'app/containers/organisation/reducer'
-import { people, peopleType } from 'app/containers/people/reducer'
+import alert from "./alert";
+import me from "./me";
+import popup from "./popup";
+import { project, projectUser } from "app/containers/project/reducer";
+import group from "app/containers/group/reducer";
+import competitor from "app/containers/competitor/reducer";
+import {
+  organisation,
+  organisationType
+} from "app/containers/organisation/reducer";
+import { people, peopleType } from "app/containers/people/reducer";
 
 const appReducer = combineReducers({
-	alert: alert,
-	popup: popup,
-	project: project,
-	organisation: organisation,
-	organisationType: organisationType,
+  alert: alert,
+  popup: popup,
+  project: project,
+  organisation: organisation,
+  organisationType: organisationType,
   people: people,
-	peopleType: peopleType,
-	group: group,
+  peopleType: peopleType,
+  group: group,
   projectUser: projectUser,
   me: me,
-	routing: routerReducer,
+  competitor: competitor,
+  routing: routerReducer
 });
 
 const rootReducer = (state, action) => {
-	// onlogout keep some of the previous state
-	if (action.type === 'USER_LOGOUT') {
-		state = {
-			setting: state.setting,
-		};
-	}
+  // onlogout keep some of the previous state
+  if (action.type === "USER_LOGOUT") {
+    state = {
+      setting: state.setting
+    };
+  }
 
-	if (action.type === 'RESET_COLLECTION') {
-		state[action.collection] = undefined;
-	}
+  if (action.type === "RESET_COLLECTION") {
+    state[action.collection] = undefined;
+  }
 
-	return appReducer(state, action);
+  return appReducer(state, action);
 };
 export default rootReducer;
