35d52d0b073fc105eee8ab34ec57418bf81ad7c4
kamrul <kamrul_sscis@hotmail.com>
2019-04-16 11:29:14 +0100

    Makeing position helper method

--- api/app/Helpers/helpers.php
+++ api/app/Helpers/helpers.php
@@ -12,7 +12,7 @@ if (!function_exists('public_path')) {
     /**
      * Get the path to the public folder.
      *
-     * @param  string $path
+     * @param string $path
      * @return string
      */
     function public_path($path = '')
@@ -102,5 +102,20 @@ if (!function_exists('getCsvString')) {
 
         $data = substr($data, 0, -1);
         return $header . "\n" . $data;
+    }
+}
+
+if (!function_exists('getPosition')) {
+    function getPosition($position)
+    {
+        if ($position > 100) {
+            return 100;
+        }
+
+        if ($position < 0) {
+            return 0;
+        }
+
+        return $position;
     }
 }
